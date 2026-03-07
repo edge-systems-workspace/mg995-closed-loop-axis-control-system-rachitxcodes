5549cc30a22b995a2a8f21b00d74f439b7cdd820
Rachit Dubey <rachitricky777@gmail.com>
2026-03-07 13:41:27 +0530

    added multiple servo angles

--- src/main.cpp
+++ src/main.cpp
@@ -13,4 +13,22 @@ void loop() {
 // write your code here
     myServo.write(0);
     delay(500);
+    myServo.write(90);
+    delay(50);
+    myServo.write(120);
+    delay(50);
+    myServo.write(180);
+    delay(50);
+    
+    // Move from 0° to 180°
+//     for (int pos = 0; pos <= 180; pos++) {
+//         myServo.write(pos);
+//         delay(50);
+//     }
+//
+//     // Move from 180° back to 0°
+//     for (int pos = 180; pos >= 0; pos--) {
+//         myServo.write(pos);
+//         delay(50);
+//     }
 }
